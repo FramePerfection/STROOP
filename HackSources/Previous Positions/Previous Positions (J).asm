5417fe0c145dfd774b5ed36d6b36c99245ea89d7
Frame <nafoizret@web.de>
2025-10-24 13:23:42 +0200

    extract common variables and functions

--- HackSources/Previous Positions/Previous Positions (J).asm
+++ HackSources/Previous Positions/Previous Positions (J).asm
@@ -1,5 +1,7 @@
 .n64
 
+ROM_VERSION equ "J"
+
 ; game functions and variables
 
 vec3f_copy equ 0x378800
@@ -30,30 +32,7 @@ MarioAngleLo equ 0x9e2e
 gWaterSurfacePseudoFloorHi equ 0x8033
 gWaterSurfacePseudoFloorLo equ 0xcb98
 
-; hack functions and variables
-
-overwrite_mario_position_and_forward_angle          equ 0x372e00
-set_next_mario_position_and_forward_angle           equ 0x372e40
-set_next_argument_position_and_mario_forward_angle  equ 0x372e80
-set_first_mario_position_and_angle                  equ 0x372ec0
-
-do_wall_collisions_for_ground_quarter_step equ 0x373380
-do_wall_collisions_for_geometry_inputs     equ 0x3733c0
-do_wall_collisions_for_air_quarter_step    equ 0x373400
-do_wall_collisions_for_water_full_step     equ 0x373420
-
-BufferOffsetHi equ 0x8037
-BufferOffsetLo equ 0x2e3c
-
-; build hack functions
-
-.create "./build/J/80372e00.bin", 0x00000000
-.include "./80372E00 (save_positions) (custom).asm"
-.close
-
-.create "./build/J/80373380.bin", 0x00000000
-.include "./80373380 (do_wall_collisions) (custom).asm"
-.close
+.include "Previous Positions (Common).asm"
 
 ; build game functions
 
--- HackSources/Previous Positions/Previous Positions (US).asm
+++ HackSources/Previous Positions/Previous Positions (US).asm
@@ -1,6 +1,8 @@
 
 .n64
 
+ROM_VERSION equ "US"
+
 ; game functions and variables
 
 vec3f_copy equ 0x378800
@@ -31,30 +33,7 @@ MarioAngleLo equ 0xb19e
 gWaterSurfacePseudoFloorHi equ 0x8033
 gWaterSurfacePseudoFloorLo equ 0xdaf8
 
-; hack functions and variables
-
-overwrite_mario_position_and_forward_angle          equ 0x372e00
-set_next_mario_position_and_forward_angle           equ 0x372e40
-set_next_argument_position_and_mario_forward_angle  equ 0x372e80
-set_first_mario_position_and_angle                  equ 0x372ec0
-
-do_wall_collisions_for_ground_quarter_step equ 0x373380
-do_wall_collisions_for_geometry_inputs            equ 0x3733c0
-do_wall_collisions_for_air_quarter_step    equ 0x373400
-do_wall_collisions_for_water_full_step     equ 0x373420
-
-BufferOffsetHi equ 0x8037
-BufferOffsetLo equ 0x2e3c
-
-; build hack functions
-
-.create "./build/US/80372E00.bin", 0x00000000
-.include "./80372E00 (save_positions) (custom).asm"
-.close
-
-.create "./build/US/80373380.bin", 0x00000000
-.include "./80373380 (do_wall_collisions) (custom).asm"
-.close
+.include "Previous Positions (Common).asm"
 
 ; build game functions
 
