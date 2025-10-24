67cc67ee8d3260cd9807df91d5bfcace2da7dc19
Frame <nafoizret@web.de>
2025-10-24 13:23:17 +0200

    finalize build script

--- HackSources/Previous Positions/create-hack-file.py
+++ HackSources/Previous Positions/create-hack-file.py
@@ -1,6 +1,7 @@
 import os
-
-rom = "J"
+import subprocess
+import glob
+from pathlib import Path
 
 def combine_bin_files(input_dir, output_file):
     with open(output_file, "w") as out:
@@ -14,7 +15,19 @@ def combine_bin_files(input_dir, output_file):
                     out.write(f"{base_name}: {hex_str}\n")
 
 if __name__ == f"__main__":
-    input_directory = f"./build/{rom}"
-    output_filename = f"./build/{rom}/Previous Positions ({rom}).hck"
-    combine_bin_files(input_directory, output_filename)
-    print(f"Combined binary data written to {output_filename}")
+    for rom in ["US", "J"]:
+        input_directory = f"./build/{rom}"
+
+        # clear potentially unwanted lingering bin files
+        bin_files = glob.glob(f'{input_directory}/*.bin')
+        for f in bin_files:
+            os.remove(f)
+
+        # assemble the code using armips
+        Path(input_directory).mkdir(parents=True, exist_ok=True)
+        subprocess.run(["armips", f"Previous Positions ({rom}).asm"])
+
+        # combine the output into the .hck file
+        output_filename = f"./build/{rom}/Previous Positions ({rom}).hck"
+        combine_bin_files(input_directory, output_filename)
+        print(f"Combined binary data written to {output_filename}")
