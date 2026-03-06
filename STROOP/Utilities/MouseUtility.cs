0040126af8dd1d9018887022c1f3aa954772b189
Frame <nafoizret@web.de>
2026-03-06 17:22:35 +0100

    use CsWin32 for 'MouseUtility'

--- STROOP/Utilities/MouseUtility.cs
+++ STROOP/Utilities/MouseUtility.cs
@@ -1,27 +1,20 @@
-﻿using System.Runtime.InteropServices;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using Windows.Win32;
+using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace STROOP.Utilities
 {
     public static class MouseUtility
     {
-        const int SM_SWAPBUTTON = 23;
-
-        [DllImport("user32.dll")]
-        static extern short GetAsyncKeyState(Keys vKey);
-
-        [DllImport("user32.dll")]
-        static extern int GetSystemMetrics(int nIndex);
-
         public static bool IsMouseDown(int button)
         {
-            bool buttonsSwapped = GetSystemMetrics(SM_SWAPBUTTON) != 0;
+            bool buttonsSwapped = PInvoke.GetSystemMetrics(SYSTEM_METRICS_INDEX.SM_SWAPBUTTON) != 0;
             if (buttonsSwapped)
                 if (button == 0) button = 1;
                 else if (button == 1) button = 0;
 
             Keys key = button == 2 ? Keys.MButton : (Keys)(button + 1);
-            return (GetAsyncKeyState(key) & 0x8000) != 0;
+            return (PInvoke.GetAsyncKeyState((int)key) & 0x8000) != 0;
         }
     }
 }
