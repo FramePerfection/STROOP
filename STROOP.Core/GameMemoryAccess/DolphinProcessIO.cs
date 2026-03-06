0d5802543c102a4b06f6735d982f151dccab2a27
Frame <nafoizret@web.de>
2026-03-06 17:58:45 +0100

    replace PInvokes in 'Kernal32NativeMethods.cs' with CsWin32 usages

--- STROOP.Core/GameMemoryAccess/DolphinProcessIO.cs
+++ STROOP.Core/GameMemoryAccess/DolphinProcessIO.cs
@@ -1,5 +1,8 @@
-﻿using System.Diagnostics;
+﻿using STROOP.Win32;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using Windows.Win32.Foundation;
+using Windows.Win32.System.ProcessStatus;
 using static STROOP.Core.Kernal32NativeMethods;
 
 namespace STROOP.Core.GameMemoryAccess;
@@ -15,12 +18,11 @@ public class DolphinProcessIO : WindowsProcessRamIO
     {
         MemoryBasicInformation info;
         IntPtr infoSize = (IntPtr)Marshal.SizeOf(typeof(MemoryBasicInformation));
-        uint setInfoSize = (uint)Marshal.SizeOf(typeof(PsapiWorkingSetExInformation));
 
         _baseOffset = (UIntPtr)0;
         bool mem1Found = false;
         for (IntPtr p = new IntPtr();
-             VQueryEx(_processHandle, p, out info, infoSize) == infoSize;
+             VirtualQueryEx(_processHandle, p, out info, infoSize) == infoSize;
              p = (IntPtr)(p.ToInt64() + info.RegionSize.ToInt64()))
         {
             if (mem1Found)
@@ -43,11 +45,9 @@ public class DolphinProcessIO : WindowsProcessRamIO
                 // exists and have nothing to do with the emulated memory. Only the right page has valid
                 // working set information so an additional check is required that it is backed by physical
                 // memory.
-                PsapiWorkingSetExInformation wsInfo;
-                wsInfo.VirtualAddress = (IntPtr)info.BaseAddress.ToUInt64();
-                if (QWorkingSetEx(_processHandle, out wsInfo, setInfoSize))
+                if (NativeMethodWrappers.QueryWorkingSetEx((HANDLE)_processHandle, info.BaseAddress, out PSAPI_WORKING_SET_EX_INFORMATION wsInfo))
                 {
-                    if ((wsInfo.VirtualAttributes & 0x01) != 0)
+                    if ((wsInfo.VirtualAttributes.Flags & 0x01) != 0)
                     {
                         _baseOffset = info.BaseAddress;
                         mem1Found = true;
---
+++ STROOP.Win32/NativeMethodWrappers.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.System.Diagnostics.Debug;
+using Windows.Win32.System.ProcessStatus;
+
+namespace STROOP.Win32;
+
+public static class NativeMethodWrappers
+{
+    public static unsafe bool ReadProcessMemory(IntPtr hProcess, UIntPtr lpBaseAddress, byte[] buffer)
+    {
+        var gcHandle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+        UIntPtr lpNumberOfBytesRead;
+        try
+        {
+            return PInvoke.ReadProcessMemory(
+                (HANDLE)hProcess,
+                (void*)lpBaseAddress,
+                (void*)Marshal.UnsafeAddrOfPinnedArrayElement(buffer, 0),
+                (uint)buffer.Length,
+                &lpNumberOfBytesRead
+            );
+        }
+        finally
+        {
+            gcHandle.Free();
+        }
+    }
+
+    public static unsafe bool WriteProcessMemory(IntPtr hProcess, UIntPtr lpBaseAddress, byte[] buffer)
+    {
+        UIntPtr numOfBytes = 0;
+        var gcHandle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+        try
+        {
+            return PInvoke.WriteProcessMemory(
+                (HANDLE)hProcess,
+                (void*)lpBaseAddress,
+                (void*)Marshal.UnsafeAddrOfPinnedArrayElement(buffer, 0),
+                (UIntPtr)buffer.Length,
+                &numOfBytes);
+        }
+        finally
+        {
+            gcHandle.Free();
+        }
+    }
+
+    public static unsafe bool QueryWorkingSetEx(IntPtr hProcess, UIntPtr lpBaseAddress, out PSAPI_WORKING_SET_EX_INFORMATION wsInfo)
+    {
+        PSAPI_WORKING_SET_EX_INFORMATION tmp;
+        tmp.VirtualAddress = (HANDLE)lpBaseAddress;
+        uint setInfoSize = (uint)Marshal.SizeOf(typeof(PSAPI_WORKING_SET_EX_INFORMATION));
+        var result = PInvoke.QueryWorkingSetEx((HANDLE)hProcess, &tmp, setInfoSize);
+        wsInfo = tmp;
+        return result;
+    }
+
+    public static unsafe bool GetSymbolAddress(SafeHandle hProcess, string name, out ulong address)
+    {
+        var symbol = new SYMBOL_INFO
+        {
+            MaxNameLen = 2000,
+            SizeOfStruct = 88,
+        };
+        var result = PInvoke.SymFromName(hProcess, name, &symbol);
+        address = symbol.Address;
+        return result;
+    }
+}
